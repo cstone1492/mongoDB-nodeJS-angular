2ab3142de5c83c5c09a733b38847955c5c9d93b9
Caroline STone <docphilstone@gmail.com>
2020-09-29 23:11:32 -0500

    added routes

--- Angular8ClientCrud/src/app/app-routing.module.ts
+++ Angular8ClientCrud/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DieSetsListComponent } from './components/die-sets-list/die-sets-list.component';
+import { DetailsComponent } from './components/die-set/details/details.component';
+import { AddDieSetComponent } from './components/add-die-set/add-die-set.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dieSets', pathMatch: 'full' },
+  { path: 'dieSets', component: DieSetsListComponent },
+  { path: 'dieSets/:id', component: DetailsComponent },
+  { path: 'add', component: AddDieSetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
